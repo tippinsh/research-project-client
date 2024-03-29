2ddeb6ea9d1bf4a044d049c0c94180b14aecb59d
Holly Tippins <hollytpns@gmail.com>
2024-03-29 11:21:02 +0000

    feat: Added button to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,27 @@
+import { useState, useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
+import { Transition } from "@headlessui/react";
 
 function HomePage() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButton(true);
+    }, 29500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white text-xl">
-      <div className="flex-col items-center justify-center">
+      <div className="flex flex-col">
         <div className="p2">
           <TypeAnimation
             style={{ whiteSpace: "pre-line" }}
             sequence={[
               `Hello, and thank you for your interest in this research.
-            This research is being conducted as part of a dissertation project.
-            The project is a requirement for being awarded a master's degree in Computer Science at the University of Northumbria.
-            This research will be overseen by the Computer and Information Sciences department.
+            The project is a requirement for being awarded a MSc degree in Computer Science at Northumbria University.
             Participation in the study is entirely voluntary and can be withdrawn at any time.
             Before deciding whether to take part, it is important for you to understand what the study involves.
     
@@ -21,9 +31,26 @@ function HomePage() {
             repeat={1}
           />
         </div>
-        <div>
-          <button className="pt-5 hidden">Next</button>
+        {/* {showButton && ( */}
+        <div className="pt-5 ml-auto">
+          <Transition
+            show={showButton}
+            enter="transition-opacity duration-1000 ease-in-out"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-600 ease-out"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <button
+              type="button"
+              class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            >
+              Next
+            </button>
+          </Transition>
         </div>
+        {/* )} */}
       </div>
     </div>
 
