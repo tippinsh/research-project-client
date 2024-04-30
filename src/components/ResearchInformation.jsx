79c8299b180ee4c832ef3265c93ea6fa2e5272a2
Holly Tippins <hollytpns@gmail.com>
2024-04-30 19:36:36 +0100

    Adding additional info to participant section

--- src/components/ResearchInformation.jsx
+++ src/components/ResearchInformation.jsx
@@ -139,6 +139,24 @@ function ResearchInformation() {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-2 text-left text-lg font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500/75">
+                <span>Disclaimer</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-gray-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-lg text-offwhite">
+                Some images depict scenes of war and may be distressing to some
+                participants. You are free to exit the study at any time.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-2 text-left text-lg font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500/75">
                 <span>Who do I contact if I have any questions</span>
                 <ChevronUpIcon
                   className={`${
